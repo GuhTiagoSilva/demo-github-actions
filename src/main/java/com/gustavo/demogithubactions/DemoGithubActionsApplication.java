4b6dba174e572684a7b2fe6f10074fcdf02c9622
Gustavo Tiago <gustavo.tiago.gts@outlook.com>
2023-04-14 21:41:24 -0300

    Initial Commit

---
+++ src/main/java/com/gustavo/demogithubactions/DemoGithubActionsApplication.java
@@ -0,0 +1,13 @@
+package com.gustavo.demogithubactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoGithubActionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoGithubActionsApplication.class, args);
+	}
+
+}
