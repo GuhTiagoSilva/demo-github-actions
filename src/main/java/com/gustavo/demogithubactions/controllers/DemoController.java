9cb711079e70b8d577847985f98353078a0fa534
Gustavo Tiago <gustavo.tiago.gts@outlook.com>
2023-04-14 22:45:02 -0300

    Adding logs

--- src/main/java/com/gustavo/demogithubactions/controllers/DemoController.java
+++ src/main/java/com/gustavo/demogithubactions/controllers/DemoController.java
@@ -1,5 +1,7 @@
 package com.gustavo.demogithubactions.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -8,15 +10,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DemoController {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(DemoController.class);
+
     @GetMapping("/")
     @ResponseStatus(HttpStatus.OK)
     public String hello() {
+        LOGGER.info("hello endpoint");
         return "Hello World!";
     }
 
     @GetMapping("/goodbye")
     @ResponseStatus(HttpStatus.OK)
     public String goodbye() {
+        LOGGER.info("goodbye endpoint");
         return "Goodbye!";
     }
 
